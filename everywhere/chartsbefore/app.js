d0bbe8386a0effa4be448922a63eec07b14380ae
David Padbury <david@davidpadbury.com>
2011-02-13 14:01:25 -0500

    adding beforecharts

---
+++ everywhere/chartsbefore/app.js
@@ -0,0 +1,15 @@
+var express = require('express'),
+		server = express.createServer();
+
+server.configure(function() {
+		server.use( express.staticProvider(__dirname + '/public') );
+		server.set('view engine', 'ejs');
+		server.set('view options', { layout: false });
+});
+
+server.get('/', function(req, res) {
+		res.render('index');
+});
+
+server.listen(3000);
+console.log('server listening on 3000');
---
+++ everywhere/chartsbefore/public/chart.js
@@ -0,0 +1,35 @@
+(function(global) {
+
+ 	function parseData(data) {
+		var items = data.split(','),
+			arr = [];
+
+		for (var i = 0; i < items.length; i++) {
+			try {
+				var v = parseFloat(items[i]);
+				arr.push(v);
+			} catch (e) {
+				// Failed to parse, just ignore..
+			}
+		}
+
+		return arr;
+	}
+
+	global.createChartOptions = function(data) {
+		var values = parseData(data);
+
+		return {
+			chart: {
+				defaultSeriesType: 'column',
+				width: 300,
+				height: 300
+			},
+			series: [{
+				data: values,
+				name: 'Number'
+			}]
+		};
+	}
+
+})(window.demo || (window.demo = {}));
