b25b384c1799295fe5db400b07f66b01f189bb8c
David Padbury <david@davidpadbury.com>
2011-06-17 22:26:41 -0400

    updating group name

--- tooling/debugging/app.js
+++ tooling/debugging/app.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(req, res) {
 	
 	res.writeHead(200, { 'Content-Type': 'text/plain' });
-	res.end('Hi ALT.NET!');
+	res.end('Hi CTDotNet!');
 
 });
 
